d424d1720ae0b62bb7014131df84654682079f23
Samuel Oduro Ansah <137756373+oduro100@users.noreply.github.com>
2024-03-22 19:54:19 +0000

    Add files via upload

---
+++ capstone-finale/updated-sock-shop/kubernetes/nginx-controller/variables.tf
@@ -0,0 +1,11 @@
+variable "domain_name" {
+  description = "This is the domain name"
+  type        = string
+  default     = "samueloduroansah.co.uk"
+}
+  
+variable "alt_domain_name" {
+  description = "This is the alternative domain name"
+  type        = string
+  default     = "*.samueloduroansah.co.uk"
+}
